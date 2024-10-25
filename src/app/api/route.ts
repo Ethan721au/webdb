3cafe4ce089f18eccb46b5a87f4fcd8700e0e44a
Ethan721au <stephane.mercier.au@gmail.com>
2024-10-25 21:30:06 +1100

    PUT

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -9,3 +9,12 @@ export async function GET() {
     email: "sdfsfs",
   });
 }
+
+export async function PUT() {
+  const player = await getPlayer();
+  return NextResponse.json({
+    name: player.name,
+    age: 25,
+    email: "sdfsfs",
+  });
+}
