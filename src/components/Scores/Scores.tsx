9a647771dafc90d11c2e780b24067dcafd5bcab0
Ethan721au <stephane.mercier.au@gmail.com>
2024-10-31 17:35:05 +1100

    changes

--- src/components/Scores/Scores.tsx
+++ src/components/Scores/Scores.tsx
@@ -6,15 +6,13 @@ export default function Scores({ players }: { players: Player[] }) {
   const scores = [...players.sort((a, b) => a.elo! - b.elo!)]
     .reverse()
     .map((player, idx) => (
-      <div key={idx}>
-        <div className={styles.row}>
-          <div
-            className={`${styles.name} ${idx === 0 ? styles.first : idx === 1 ? styles.second : idx === 2 ? styles.third : ""}`}
-          >
-            {player.first_name}
-          </div>
-          <div className={styles.score}>{player.elo}</div>
+      <div className={styles.row} key={idx}>
+        <div
+          className={`${styles.name} ${idx === 0 ? styles.first : idx === 1 ? styles.second : idx === 2 ? styles.third : ""}`}
+        >
+          {player.first_name}
         </div>
+        <div className={styles.score}>{player.elo}</div>
       </div>
     ));
 
