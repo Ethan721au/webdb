fddb6a0beb62f806ce4636be4eff9aca2494771a
Ethan721au <stephane.mercier.au@gmail.com>
2024-11-02 19:19:02 +1100

    updates

--- src/lib/calculateWinRates.ts
+++ src/lib/calculateWinRates.ts
@@ -5,11 +5,20 @@ export const calculateWinRates = (
   winner: Player,
   loser: Player
 ) => {
-  console.log(winner, loser);
   const opponents = [winner, loser];
-  console.log(opponents);
 
   const updatedWinRates = opponents.map((player) => {
+    if (!player.winRates) {
+      return {
+        ...player,
+        winRates: {
+          globalWinRate: 0,
+          globalWins: 0,
+          globalLosses: 0,
+          vs: [],
+        },
+      };
+    }
     const { globalWins, globalLosses } = player.winRates;
     const updatedGlobalWins = player === winner ? globalWins + 1 : globalWins;
     const updatedGlobalLosses =
@@ -24,7 +33,7 @@ export const calculateWinRates = (
         globalWins: updatedGlobalWins,
         globalLosses: updatedGlobalLosses,
         globalWinRate: updatedGlobalWinRate,
-        vs: player.winRates.vs.map((opponent) => {
+        vs: player.winRates!.vs.map((opponent) => {
           if (opponent.player === winner.first_name) {
             const updatedWins = opponent.wins;
             const updatedLosses = opponent.losses + 1;
@@ -57,52 +66,6 @@ export const calculateWinRates = (
     };
   });
 
-  // const updatedWinRates = opponents.map((player) => {
-  //   const { globalWins, globalLosses } = player.winRates;
-  //   const updatedGlobalWins = player === winner ? globalWins + 1 : globalWins;
-  //   const updatedGlobalLosses =
-  //     player === loser ? globalLosses + 1 : globalLosses;
-  //   const updatedGlobalWinRate =
-  //     Math.round(
-  //       (updatedGlobalWins / (updatedGlobalWins + updatedGlobalLosses)) * 100
-  //     ) / 100;
-  //   return {
-  //     globalWins: updatedGlobalWins,
-  //     globalLosses: updatedGlobalLosses,
-  //     globalWinRate: updatedGlobalWinRate,
-  //     vs: player.winRates.vs.map((opponent) => {
-  //       if (opponent.player === winner.first_name) {
-  //         const updatedWins = opponent.wins;
-  //         const updatedLosses = opponent.losses + 1;
-  //         const updatedWinRate =
-  //           Math.round((updatedWins / (updatedWins + updatedLosses)) * 100) /
-  //           100;
-  //         return {
-  //           player: opponent.player,
-  //           wins: updatedWins,
-  //           losses: updatedLosses,
-  //           winRate: updatedWinRate,
-  //         };
-  //       }
-  //       if (opponent.player === loser.first_name) {
-  //         const updatedWins = opponent.wins + 1;
-  //         const updatedLosses = opponent.losses;
-  //         const updatedWinRate =
-  //           Math.round((updatedWins / (updatedWins + updatedLosses)) * 100) /
-  //           100;
-  //         return {
-  //           player: opponent.player,
-  //           wins: updatedWins,
-  //           losses: updatedLosses,
-  //           winRate: updatedWinRate,
-  //         };
-  //       }
-  //       return opponent;
-  //     }),
-  //   };
-  // });
-
-  console.log(updatedWinRates, "updatedWinRates");
   const test = updatedWinRates.filter((p) => {
     return p.first_name === player.first_name;
   });
