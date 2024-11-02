564f7540e71793eca9a525dc361c93f4e7ea72e9
Ethan721au <stephane.mercier.au@gmail.com>
2024-11-02 19:11:47 +1100

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 // import GamesSummary from "@/components/GamesSummary/GamesSummary";
 // import Scores from "@/components/Scores/Scores";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import styles from "./home.module.css";
 import { playersSeed } from "@/config/seeds/playersSeed";
 import { determineEloExchange } from "@/lib/calculateEloExchange";
@@ -11,77 +11,89 @@ import { Player } from "@/types";
 // import PlayerForm from "@/components/PlayerForm/PlayerForm";
 
 export default function Home() {
-  const [games, setGames] = useState([]);
-  const [players, setPlayers] = useState([]);
+  // const [games, setGames] = useState([]);
+  // const [players, setPlayers] = useState([]);
   // const [message, setMessage] = useState("");
-  const [message2, setMessage2] = useState("");
-  const [winner, setWinner] = useState(playersSeed[0]);
-  const [loser, setLoser] = useState(playersSeed[4]);
-  const [draw, setDraw] = useState(true);
+  // const [message2, setMessage2] = useState("");
+  // const [winner, setWinner] = useState(playersSeed[0]);
+  // const [loser, setLoser] = useState(playersSeed[4]);
+  // const [draw, setDraw] = useState(false);
 
   useEffect(() => {
-    getAllGames();
-    getAllPlayers();
+    // getAllGames();
+    // getAllPlayers();
   }, []);
 
-  const getAllGames = async () => {
-    const data = await fetch("http://localhost:3000/api/games", {
-      method: "GET",
+  // const getAllGames = async () => {
+  //   const data = await fetch("http://localhost:3000/api/games", {
+  //     method: "GET",
+  //   });
+
+  //   const { games } = await data.json();
+  //   setGames(games);
+  // };
+
+  // const getAllPlayers = async () => {
+  //   const data = await fetch("http://localhost:3000/api/players", {
+  //     method: "GET",
+  //   });
+
+  //   const { players } = await data.json();
+  //   setPlayers(players);
+  // };
+
+  const updatePlayer = async (players: Player[]) => {
+    players.map(async (player) => {
+      const res = await fetch("http://localhost:3000/api/players", {
+        method: "PUT",
+        body: JSON.stringify(player),
+      });
+      const message = await res.json();
+      console.log(message, "message players");
     });
-
-    const { games } = await data.json();
-    setGames(games);
-  };
-
-  const getAllPlayers = async () => {
-    const data = await fetch("http://localhost:3000/api/players", {
-      method: "GET",
-    });
-
-    const { players } = await data.json();
-    setPlayers(players);
   };
 
   const recordResult = async () => {
-    const gameDetails = {
-      season: 8,
-      opponents: [winner, loser],
-      isDraw: draw ? true : false,
-      winner: draw ? null : winner,
-      loser: draw ? null : loser,
-      eloExchange: draw ? null : determineEloExchange(winner.elo!, loser.elo!),
-    };
+    const opponents = [playersSeed[0], playersSeed[4]];
+    const draw = false;
+    const eloExchange = determineEloExchange(
+      opponents[0].elo!,
+      opponents[1].elo!
+    );
+    const season = 8;
+    const winner = opponents[0];
+    const loser = opponents[1];
 
-    const updatedOpponents = gameDetails.opponents.map((player) => {
+    const updatedOpponents = opponents.map((player) => {
       return {
         ...player,
         elo: draw
           ? player.elo
-          : player === gameDetails.winner
-            ? player.elo! + gameDetails!.eloExchange!
-            : player.elo! - gameDetails!.eloExchange!,
+          : player === winner
+            ? player.elo! + eloExchange!
+            : player.elo! - eloExchange!,
         winRates: draw
           ? player.winRates
-          : calculateWinRates(player, gameDetails.winner, gameDetails.loser),
+          : calculateWinRates(player, winner!, loser!),
         leaderboard: player.leaderboard?.map((entry) => {
-          if (entry.season !== gameDetails.season) return entry;
+          if (entry.season !== season) return entry;
           return {
             ...entry,
             matchesPlayed: entry.matchesPlayed + 1,
             wins: draw
               ? entry.wins
-              : player === gameDetails.winner
+              : player === winner
                 ? entry.wins + 1
                 : entry.wins,
             losses: draw
               ? entry.losses
-              : player === gameDetails.loser
+              : player === loser
                 ? entry.losses + 1
                 : entry.losses,
             draws: draw ? entry.draws + 1 : entry.draws,
             overall_points: draw
               ? entry.overall_points + 1
-              : player === gameDetails.winner
+              : player === winner
                 ? entry.overall_points + 3
                 : entry.overall_points,
           };
@@ -89,8 +101,28 @@ export default function Home() {
       };
     });
 
+    const gameDetails = {
+      season: season,
+      opponents: updatedOpponents,
+      isDraw: draw ? true : false,
+      winner: draw
+        ? null
+        : updatedOpponents.find((player) => player === winner),
+      loser: draw ? null : updatedOpponents.find((player) => player === loser),
+      eloExchange: draw ? null : eloExchange,
+    };
+
     console.log(updatedOpponents, "updatedOpponents");
     console.log(gameDetails, "gameDetails");
+
+    const data = await fetch("http://localhost:3000/api/games", {
+      method: "POST",
+      body: JSON.stringify(gameDetails),
+    });
+    const message = await data.json();
+    console.log(message, "message game");
+
+    updatePlayer(updatedOpponents);
 
     // const updatedPlayer1 = {
     //   ...loser,
@@ -131,7 +163,7 @@ export default function Home() {
       {/* <PlayerForm /> */}
       <button onClick={recordResult}>test</button>
       {/* <div>{message}</div> */}
-      <div>{message2}</div>
+      {/* <div>{message2}</div> */}
     </div>
   );
 }
