97da6db434b340cd1e9c1bbbdafff24270889083
Ethan721au <stephane.mercier.au@gmail.com>
2024-11-04 20:34:52 +1100

    chnages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./home.module.css";
+// import { calculateElo } from "@/lib/calculatePlayerElo";
+// import { calculateWinRates } from "@/lib/calculateWinRates";
+// import { updateLeaderboardEntry } from "@/lib/updateLeaderboardEntry";
+// import { determineEloExchange } from "@/lib/calculateEloExchange";
 
 // import { Player } from "@/types";
 
+// const draw = false;
+// const season = 8;
+
 export default function Home() {
   // const [games, setGames] = useState([]);
-  // const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState([]);
+  console.log(players, "players");
 
   useEffect(() => {
     // getAllGames();
-    // getAllPlayers();
+    getAllPlayers();
   }, []);
 
   // const getAllGames = async () => {
@@ -23,58 +31,71 @@ export default function Home() {
   //   setGames(games);
   // };
 
-  // const getAllPlayers = async () => {
-  //   const data = await fetch("http://localhost:3000/api/players", {
-  //     method: "GET",
-  //   });
+  const getAllPlayers = async () => {
+    const data = await fetch("http://localhost:3000/api/players", {
+      method: "GET",
+    });
 
-  //   const { players } = await data.json();
-  //   setPlayers(players);
-  // };
+    const { players } = await data.json();
+    setPlayers(players);
+  };
 
   // const updatePlayer = async () => {
-
   //   const playersToUpdate = players.filter(
   //     (player) => player.first_name === "Eric" || player.first_name === "Jane"
   //   );
 
-  //   console.log(playersToUpdate, "playersToUpdate");
   //   const [winner, loser] = [playersToUpdate[0], playersToUpdate[1]];
+  //   console.log(winner, "winner");
+  //   const eloExchange = determineEloExchange(winner!.elo!, loser!.elo!);
 
-  //   playersToUpdate.map((player) => {
-  //     const opponent = player === winner ? loser : winner;
-  //     console.log(player, "player");
-  //     console.log("ok");
-  //     if (
-  //       !player.winRates?.vs ||
-  //       !player.winRates?.vs.find((p) => p.player === opponent?.first_name)
-  //     ) {
-  //       console.log(player.first_name, "player.first_name");
+  //   const updatedOpponents = [winner, loser].map((player) => {
+  //     const updatedPlayer = [winner, loser].map((player) => {
+  //       const opponent = player === winner ? loser : winner;
+  //       if (
+  //         !player.winRates?.vs ||
+  //         !player.winRates?.vs.find((p) => p.player === opponent?.first_name)
+  //       ) {
+  //         const newVs = {
+  //           player: opponent?.first_name,
+  //           wins: 0,
+  //           losses: 0,
+  //           winRate: 0,
+  //         };
 
-  //       const newVs = {
-  //         player: opponent?.first_name,
-  //         wins: 0,
-  //         losses: 0,
-  //         winRate: 0,
-  //       };
+  //         const updatedPlayer = {
+  //           ...player,
+  //           winRates: {
+  //             ...player.winRates,
+  //             vs: [...player.winRates?.vs, newVs],
+  //           },
+  //         };
 
-  //       const updatedPlayer = {
-  //         ...player,
-  //         winRates: {
-  //           ...player.winRates,
-  //           vs: [...player.winRates?.vs, newVs],
-  //         },
-  //       };
+  //         return updatedPlayer;
+  //       }
 
-  //       console.log(updatedPlayer, "updatedPlayer");
-  //     }
+  //       return player;
+  //     });
+
+  //     console.log(updatedPlayer, "updatedPlayer");
+  //     return {
+  //       ...player,
+  //       elo: calculateElo(player, eloExchange, draw, winner),
+  //       winRates: draw
+  //         ? player.winRates
+  //         : calculateWinRates(player, updatedPlayer),
+  //       leaderboard: player?.leaderboard?.map((entry) =>
+  //         updateLeaderboardEntry(entry, player, season, draw, winner, loser)
+  //       ),
+  //     };
   //   });
 
+  //   console.log(updatedOpponents, "updatedOpponents");
   // };
 
   return (
     <div className={styles.container}>
-      {/* <button onClick={updatePlayer}>test</button> */}
+      <button>test</button>
     </div>
   );
 }
