f8b75f9b2ba581ba6ef5d822b13e2cbda9882b11
Ethan721au <stephane.mercier.au@gmail.com>
2024-11-07 17:26:21 +1100

    updated

--- src/components/CountersConfig/CountersConfig.tsx
+++ src/components/CountersConfig/CountersConfig.tsx
@@ -1,13 +1,13 @@
-import styles from "./countersConfig.module.css";
+// import styles from "./countersConfig.module.css";
 
-export default function CountersConfig({ setLife }: { setLife: any }) {
-  return (
-    <div className={styles.container}>
-      <div>timer</div>
-      <button onClick={() => setLife(20)}>reset</button>
-      <div>config</div>
-      <div>config</div>
-      <div>config</div>
-    </div>
-  );
-}
+// export default function CountersConfig({ setLife }: { setLife: any }) {
+//   return (
+//     <div className={styles.container}>
+//       <div>timer</div>
+//       <button onClick={() => setLife(20)}>reset</button>
+//       <div>config</div>
+//       <div>config</div>
+//       <div>config</div>
+//     </div>
+//   );
+// }
